820900e7ae975120a0f65b3cfb7b92f67a5316ce
Sydney Ly <sydly@berkeley.edu>
2025-06-16 13:30:05 -0700

    completed lab2

---
+++ lab2.py
@@ -0,0 +1,21 @@
+# Lab 2: Python Profiles
+
+## Step 1
+name = "Sydney Ly"
+
+## Step 2
+nickname = 'Syd'
+
+## Step 3
+age = 23
+
+## Step 4
+used_python = True
+
+## Step 5
+hobbies = ["reading", "cooking", "playing videogames", "walking my dog", "playing pickleball", "hanging out with friends"]
+
+## Step 6
+favorite_things = {"movie": "across the spiderverse", 'food': 'sushi', 'animal': 'dogs', "color": 'blue', 'sport': 'gymnastics', 'tv show': 'avatar the last airbender'}
+
+## Optional: add your own variables here!
